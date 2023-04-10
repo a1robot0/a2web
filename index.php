183955f83e37803eb641a1880a495ad421c827af
a1robot <128459746+a1robot0@users.noreply.github.com>
2023-04-10 21:42:27 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Page</title>
+</head>
+<body>
+    <h1 style="text-align:center;">Welcome to my Page!</h1>
+    <p style="text-align:center;">Today is <?php echo date('Y-m-d'); ?></p>
+    <p style="text-align:center;">The current time is <?php echo date('H:i:s'); ?></p>
+    <a href="https://aiworkcn.com/a1web/a1web2" target="_blank">
+      <img src="https://source.unsplash.com/random/1300x600" alt="banner image" style="display: block; margin: 0 auto; max-width: 100%; height: auto; position: relative; z-index: 0;" loading="lazy">
+    </a>
+</body>
+</html>
